723f2f43e12972754f58f78e57e1747588790ab8
Сергей Краснослободцев <serega-aleks23@outlook.com>
2024-06-09 19:35:33 +0300

    Fast_frame

--- face_descriptor.py
+++ face_descriptor.py
@@ -5,11 +5,13 @@ from sklearn.decomposition import PCA
 
 face_rec_model = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
 
+
 def get_face_descriptor(img, shape):
     return np.array(face_rec_model.compute_face_descriptor(img, shape))
 
 
 pca = PCA(n_components=50)
+
 
 def apply_pca_to_face(img, face):
     """
@@ -23,10 +25,11 @@ def apply_pca_to_face(img, face):
         numpy.ndarray: Уменьшенное изображение лица.
     """
     x, y, w, h = face.left(), face.top(), face.width(), face.height()
-    face_img = img[y:y+h, x:x+w]
+    face_img = img[y:y + h, x:x + w]
     face_img_resized = cv2.resize(face_img, (100, 100)).flatten()  # Изменение размера и преобразование в вектор
     face_img_pca = pca.fit_transform([face_img_resized])  # Применение PCA
     return face_img_pca
+
 
 def get_face_descriptor_with_pca(img, shape):
     """
--- face_detector.py
+++ face_detector.py
@@ -1,18 +1,20 @@
 import dlib
 import cv2
 from mtcnn_cv2 import MTCNN
-from helpers.converters import convert_and_trim_bb
 
 predictor = dlib.shape_predictor('models/face_landmarks_68.dat')
+
 
 def detect_faces_dlib(img):
     detector = dlib.get_frontal_face_detector()
     faces = detector(img)
     shapes = [predictor(img, face) for face in faces]
-    
+
     return faces, shapes
 
-cnn_model_path = 'models\mmod_human_face_detector.dat'
+
+cnn_model_path = 'models/mmod_human_face_detector.dat'
+
 
 def detect_faces_dlib_cnn(img):
     detector = dlib.cnn_face_detection_model_v1(cnn_model_path)
@@ -21,22 +23,25 @@ def detect_faces_dlib_cnn(img):
     faces = [face.rect for face in faces]
     return faces, shapes
 
-haar_cascade = cv2.CascadeClassifier('models\haarcascade_frontalface_default.xml')
+
+haar_cascade = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+
 
 def detect_faces_haar(img):
     detections = haar_cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
-    faces = [dlib.rectangle(int(x), int(y), int(x+w), int(y+h)) for (x, y, w, h) in detections]
+    faces = [dlib.rectangle(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in detections]
     shapes = [predictor(img, face) for face in faces]
-    
+
     return faces, shapes
 
 
 mtcnn_detector = MTCNN()
 
+
 def detect_faces_mtcnn(img):
     detections = mtcnn_detector.detect_faces(img)
-    faces = [dlib.rectangle(det['box'][0], det['box'][1], det['box'][0] + det['box'][2], det['box'][1] + det['box'][3]) for det in detections]
+    faces = [dlib.rectangle(det['box'][0], det['box'][1], det['box'][0] + det['box'][2], det['box'][1] + det['box'][3])
+             for det in detections]
     shapes = [predictor(img, face) for face in faces]
 
     return faces, shapes
-
